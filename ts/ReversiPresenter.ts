31e3157c21a20e33e2eb657538270aa1a3234585
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-02-19 18:47:14 +0100

    up

--- ts/ReversiPresenter.ts
+++ ts/ReversiPresenter.ts
@@ -1,3 +1,4 @@
+import { fromEvent } from 'rxjs';
 import {GameState, ReversiModelInterface} from './ReversiDefinitions';
 
 const reversiView = `
@@ -10,7 +11,10 @@ export class ReversiPresenter {
     tds: HTMLElement[][] = [];
 
     constructor(private root: HTMLElement, private model: ReversiModelInterface) {
+        root.innerHTML = reversiView;
         this.initBoard();
+        // Traduire le Modèle vers la Vue
+        model.gameStateObs.subscribe( gs => this.updatePresentation(gs) );
     }
 
     updatePresentation({board, turn}: GameState) {
@@ -18,6 +22,14 @@ export class ReversiPresenter {
         // Les cases (balises td) contenant un pion du joueur 1 ont la classe CSS Player1 (voir le CSS)
         // Les cases (balises td) contenant un pion du joueur 2 ont la classe CSS Player2 (voir le CSS)
         // Les cases sur lesquelles le joueur courant peut poser un pion ont la classe CSS canPlayHere
+        for(let i=0; i<8; i++) {​​
+            for(let j=0; j<8; j++) {​​
+                this.tds[i][j].className = board[i][j];
+                 if (this.model.PionsTakenIfPlayAt(i, j).length > 0) {
+                    this.tds[i][j].classList.add('canPlayHere');
+                 }
+            }​​
+        }​​
     }
 
     private initBoard() {
@@ -25,5 +37,25 @@ export class ReversiPresenter {
         // Remplir le tableau avec 8x8 cases contenant chacune une balise div
         // Utiliser la fonction document.createElement
         // Stockez les balises td dans l'attribut tds de l'objet => ça vous facilitera la vie plus tard
-    }
+        const tbody = this.root.querySelector('tbody') as HTMLTableSectionElement;
+        const V = Array(8).fill(0);
+        tbody.innerHTML = V.map( () => `
+            <tr>
+                ${V.map( () => '<td><div></div></td>').join('')}
+            </tr>
+        `
+        ).join('');
+
+        // Récupérer les balises td dans tds
+        this.tds = Array.from( tbody.querySelectorAll('tr') ).map(
+            (tr, i) => {
+                const tab = Array.from( tr.querySelectorAll('td') );
+                tab.forEach( (td, j) => fromEvent(td, 'click').subscribe(
+                    () => this.model.play(i, j)
+                ));
+                return tab;
+            }
+        );
+    }​​
+
 }
