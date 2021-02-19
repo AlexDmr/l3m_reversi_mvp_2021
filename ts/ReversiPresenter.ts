2c8335a503200a25af3d18b9dad191232ab6ebd6
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-02-19 22:41:56 +0100

    mise à jour correction initBoard

--- ts/ReversiPresenter.ts
+++ ts/ReversiPresenter.ts
@@ -38,24 +38,31 @@ export class ReversiPresenter {
         // Utiliser la fonction document.createElement
         // Stockez les balises td dans l'attribut tds de l'objet => ça vous facilitera la vie plus tard
         const tbody = this.root.querySelector('tbody') as HTMLTableSectionElement;
+        for (let i=0; i<8; i++) {
+            const L: HTMLElement[] = [];
+            const tr = document.createElement("tr");
+            tbody.appendChild(tr);
+            for(let j=0; j<8; j++) {
+                const td = document.createElement("td");
+                tr.appendChild(td);
+                L.push(td);
+                td.appendChild( document.createElement("div") );
+                td.onclick = () => this.model.play(i, j);
+            }
+            this.tds.push(L);
+        }
+
+        /*  OU
         const V = Array(8).fill(0);
-        tbody.innerHTML = V.map( () => `
-            <tr>
-                ${V.map( () => '<td><div></div></td>').join('')}
-            </tr>
-        `
-        ).join('');
+        tbody.innerHTML = V.map( () => `<tr>  ${V.map( () => '<td><div></div></td>').join('')}  </tr> `).join('');
 
         // Récupérer les balises td dans tds
-        this.tds = Array.from( tbody.querySelectorAll('tr') ).map(
-            (tr, i) => {
+        this.tds = Array.from( tbody.querySelectorAll('tr') ).map( (tr, i) => {
                 const tab = Array.from( tr.querySelectorAll('td') );
-                tab.forEach( (td, j) => fromEvent(td, 'click').subscribe(
-                    () => this.model.play(i, j)
-                ));
+                tab.forEach( (td, j) => td.onclick = () => this.model.play(i, j) );
                 return tab;
             }
-        );
+        );*/
     }​​
 
 }
